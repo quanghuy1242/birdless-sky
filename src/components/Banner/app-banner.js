f879c887f22a1f266f295d851396bcf6e6402b90
quanghuy1242 <katygaga124@gmail.com>
2019-10-02 17:48:59 +0700

    update banner style

--- src/components/Banner/app-banner.js
+++ src/components/Banner/app-banner.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css, property, customElement, unsafeCSS, queryAll } from 'lit-element';
+import { LitElement, html, css, property, customElement, unsafeCSS, queryAll, query } from 'lit-element';
 import { mdcTypographyStyles, mdcButtonStyles } from '../../sharestyles';
 import style from './app-banner.scss';
 import { MDCRipple } from '@material/ripple';
@@ -6,9 +6,11 @@ import { MDCRipple } from '@material/ripple';
 @customElement('app-banner')
 export class AppMain extends LitElement {
   @queryAll('.mdc-button') buttonElements;
+  @query('.banner-wrapper') bannerWrapper;
   
   @property({ type: String }) name = 'Quang Huy';
   @property({ type: String }) slogan = 'Quang Huy';
+  @property({ type: String }) image = '';
 
   static get styles() {
     return [
@@ -19,9 +21,16 @@ export class AppMain extends LitElement {
   }
 
   firstUpdated() {
+    this.setBackgroundImage(this.image);
     this.buttonElements.forEach(buttonElement => {
       MDCRipple.attachTo(buttonElement);
     });
+  }
+
+  setBackgroundImage(image) {
+    if (image) {
+      this.bannerWrapper.style.backgroundImage = `url(${image})`
+    }
   }
 
   render() {
@@ -29,7 +38,7 @@ export class AppMain extends LitElement {
       <div class="banner-wrapper">
         <h1 class="mdc-typography--headline2">${this.name}</h1>
         <div class="mdc-button mdc-button--unelevated mdc-typography--body1 slogan">${this.slogan}</div>
-        <a href='/about' class="mdc-button mdc-button--unelevated btn-learn-about">Learn About Me</a>
+        <a href='/about' class="mdc-button mdc-button--raised btn-learn-about">Learn About Me</a>
       </div>
     `;
   }
