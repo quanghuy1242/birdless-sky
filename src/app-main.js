6e9880cee74a2fe985c0ff7102fecfb34ebca9c9
quanghuy1242 <katygaga124@gmail.com>
2019-10-02 10:16:20 +0700

    rename and ...

--- src/app-main.js
+++ src/app-main.js
@@ -1,17 +1,24 @@
-import { LitElement, html, css, property, customElement, query } from 'lit-element';
+import { LitElement, html, css, property, customElement, query, unsafeCSS } from 'lit-element';
 import { connect } from 'pwa-helpers';
 import { connectRouter, navigate } from 'lit-redux-router';
 import { store } from './store/index';
+import style from './app-main.scss';
 
 import './Pages/Home/app-home';
 import './Pages/Post/app-post';
 import './Pages/NotFound/app-not-found';
 import './Pages/About/app-about';
 
-import './components/NavTop/app-nav-top';
+import './components/NavTop/app-main-content';
 
 @customElement('app-main')
 export class AppMain extends LitElement {
+  static get styles() {
+    return [
+      css`${unsafeCSS(style)}`
+    ];
+  }
+  
   constructor() {
     super();
     connectRouter(store);
@@ -19,18 +26,19 @@ export class AppMain extends LitElement {
 
   render() {
     return html`
-      <app-nav-top></app-nav-top>
-      <nav>
-        <a href="/">Home</a>
-        <a href="/post/1">Blog số 1</a>
-        <a href="/about">About</a>
-      </nav>
-      <div class="outlet">
-        <lit-route path="/" component="app-home"></lit-route>
-        <lit-route path="/post/:id" component="app-post"></lit-route>
-        <lit-route path="/about" component="app-about"></lit-route>
-        <lit-route component="app-not-found"></lit-route>
-      </div>
+      <app-main-content>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/post/1">Blog số 1</a>
+          <a href="/about">About</a>
+        </nav>
+        <div class="outlet">
+          <lit-route path="/" component="app-home"></lit-route>
+          <lit-route path="/post/:id" component="app-post"></lit-route>
+          <lit-route path="/about" component="app-about"></lit-route>
+          <lit-route component="app-not-found"></lit-route>
+        </div>
+      </app-main-content>
     `;
   }
 }
--- src/components/NavTop/app-main-content.js
+++ src/components/NavTop/app-main-content.js
@@ -1,15 +1,17 @@
-import { LitElement, html, css, property, customElement, query, queryAll } from 'lit-element';
+import { LitElement, html, css, property, customElement, query, queryAll, unsafeCSS } from 'lit-element';
 import { mdcTopAppBarStyles, mdcButtonStyles } from '../../sharestyles';
 import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCRipple } from '@material/ripple';
+import style from './app-main-content.scss';
 
-@customElement('app-nav-top')
+@customElement('app-main-content')
 export class AppNavTop extends LitElement {
   @query('.mdc-top-app-bar') topAppBarElement;
   @queryAll('.mdc-button') buttonElements;
 
   static get styles() {
     return [
+      css`${unsafeCSS(style)}`,
       mdcTopAppBarStyles,
       mdcButtonStyles
     ];
@@ -25,18 +27,23 @@ export class AppNavTop extends LitElement {
 
   render() {
     return html`
-      <header class="mdc-top-app-bar">
+      <header class="mdc-top-app-bar app-bar">
         <div class="mdc-top-app-bar__row">
           <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-            <button class="mdc-button mdc-button--unelevated">Birdless Sky</button>
+            <a href="/" class="mdc-button mdc-button--unelevated">Birdless Sky</a>
           </section>
           <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
-            <button class="mdc-button mdc-button--unelevated">Find me</button>
-            <button class="mdc-button mdc-button--unelevated">Sign in</button>
-            <button class="mdc-button mdc-button--unelevated">Register</button>
+            <a href="/about" class="mdc-button mdc-button--unelevated">Find me</a>
+            <a href="/post/1" class="mdc-button mdc-button--unelevated">Sign in</a>
+            <a href="/something" class="mdc-button mdc-button--unelevated">Register</a>
           </section>
         </div>
       </header>
+      <div class="mdc-drawer-app-content mdc-top-app-bar--fixed-adjust">
+        <main class="main-content" id="main-content">
+          <slot></slot>
+        </main>
+      </div>
     `;
   }
 }
