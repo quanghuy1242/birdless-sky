7249c999e28489e1b144d8010892df53bd4bf96a
quanghuy1242 <katygaga124@gmail.com>
2019-10-04 00:20:08 +0700

    use new component

--- src/pages/Home/app-home.js
+++ src/pages/Home/app-home.js
@@ -1,6 +1,8 @@
 import { LitElement, html, css, property, customElement } from 'lit-element';
 import { updateMetadata } from 'pwa-helpers';
 
+import '../../components/CircularProgress/app-circular-progress';
+
 @customElement('app-home')
 export class AppMain extends LitElement {
   updated() {
@@ -15,6 +17,7 @@ export class AppMain extends LitElement {
     return html`
       <div class="home-wrapper">
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi, omnis sapiente velit commodi dolorum non explicabo. Corporis ad, reprehenderit odit excepturi, dolorem eos minima nam ullam ea corrupti, quasi vitae!</p>
+        <app-circular-progress size="xlarge" center></app-circular-progress>
       </div>
     `;
   }
