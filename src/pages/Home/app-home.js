a5f257690233f0e32284403a5a07507647e4674b
quanghuy1242 <katygaga124@gmail.com>
2019-10-04 15:06:29 +0700

    vertical algin center progress

--- src/pages/Home/app-home.js
+++ src/pages/Home/app-home.js
@@ -1,10 +1,17 @@
-import { LitElement, html, css, property, customElement } from 'lit-element';
+import { LitElement, html, css, property, customElement, unsafeCSS } from 'lit-element';
 import { updateMetadata } from 'pwa-helpers';
+import style from './app-home.scss';
 
 import '../../components/CircularProgress/app-circular-progress';
 
 @customElement('app-home')
 export class AppMain extends LitElement {
+  static get styles() {
+    return [
+      css`${unsafeCSS(style)}`
+    ];
+  }
+
   updated() {
     updateMetadata({
       title: 'Home',
@@ -16,7 +23,7 @@ export class AppMain extends LitElement {
   render() {
     return html`
       <div class="home-wrapper">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi, omnis sapiente velit commodi dolorum non explicabo. Corporis ad, reprehenderit odit excepturi, dolorem eos minima nam ullam ea corrupti, quasi vitae!</p>
+        
         <app-circular-progress size="xlarge" center></app-circular-progress>
       </div>
     `;
