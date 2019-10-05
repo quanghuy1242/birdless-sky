51627559a88adc07829b2a7c36d31d1b272d1ee7
quanghuy1242 <katygaga124@gmail.com>
2019-10-05 22:33:58 +0700

    added template button more

--- src/components/PostList/app-post-list.js
+++ src/components/PostList/app-post-list.js
@@ -1,17 +1,22 @@
-import { LitElement, html, property, customElement, css, unsafeCSS } from 'lit-element';
+import { LitElement, html, property, customElement, css, unsafeCSS, query, queryAll } from 'lit-element';
 import { connect } from 'pwa-helpers';
 import { store } from '../../store';
 import { fetchAllPosts } from '../../store/actions/post';
 import style from './app-post-list.scss';
+import { mdcButtonStyles } from '../../sharestyles';
 
 import '../../components/CardItem/app-card-item';
+import { MDCRipple } from '@material/ripple';
 
 @customElement('app-post-list')
 export class AppPostList extends connect(store)(LitElement) {
+  @queryAll('.mdc-button') buttonMore;
+
   @property({ type: Array }) posts;
 
   static get styles() {
     return [
+      mdcButtonStyles,
       css`${unsafeCSS(style)}`,
     ];
   }
@@ -21,6 +26,9 @@ export class AppPostList extends connect(store)(LitElement) {
   }
 
   firstUpdated() {
+    this.buttonMore.forEach(element => {
+      MDCRipple.attachTo(element);
+    });
     store.dispatch(fetchAllPosts());
   }
 
@@ -35,6 +43,7 @@ export class AppPostList extends connect(store)(LitElement) {
           description=${post.preview}
         ></app-card-item>
       `)}
+      <button class="mdc-button mdc-button--raised">Load more</button>
     `;
   }
 }
