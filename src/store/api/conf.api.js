9f5938367ab699e55121b4f1035bedcd8b0d3c64
quanghuy1242 <katygaga124@gmail.com>
2019-10-05 17:58:38 +0700

    delete api

--- src/store/api/conf.api.js
+++
@@ -1,6 +0,0 @@
-import { db } from '../../firebase';
-
-export const getConfigFromServer = async () => {
-  const dataSnapshot = await db.collection('conf').get();
-  return dataSnapshot.docs.map(doc => doc.data())[0];
-}
