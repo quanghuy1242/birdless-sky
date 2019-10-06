d36e336348ec9198db47a9ad1a644686a9ef34f2
quanghuy1242 <katygaga124@gmail.com>
2019-10-06 14:31:33 +0700

    modal drawer on mobile

--- src/components/NavTop/app-main-content.js
+++ src/components/NavTop/app-main-content.js
@@ -6,13 +6,15 @@ import {
   mdcTypographyStyles,
   mdcListStyles,
   mdcDrawerStyles,
-  materialIconsStyles
+  materialIconsStyles,
+  mdcIconButtonStyles
 } from '../../sharestyles';
 import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCRipple } from '@material/ripple';
 import { MDCList } from '@material/list';
-// import { MDCDrawer } from '@material/drawer';
-import { connect } from 'pwa-helpers';
+import { MDCDrawer } from '@material/drawer';
+import { connect, installMediaQueryWatcher } from 'pwa-helpers';
+import { classMap } from 'lit-html/directives/class-map';
 import { store } from '../../store';
 import style from './app-main-content.scss';
 
@@ -30,6 +32,7 @@ export class AppNavTop extends connect(store)(LitElement) {
   @query('app-banner') bannerElement;
 
   @property({ type: String }) pathname = window.location.pathname;
+  @property({ type: Boolean }) isMobile;
 
   static get styles() {
     return [
@@ -40,8 +43,15 @@ export class AppNavTop extends connect(store)(LitElement) {
       mdcListStyles,
       mdcDrawerStyles,
       materialIconsStyles,
+      mdcIconButtonStyles,
       css`${unsafeCSS(style)}`,
     ];
+  }
+
+  constructor() {
+    super();
+    this.excuteQueryMatchers();
+    window.addEventListener('resize', this.excuteQueryMatchers);
   }
 
   stateChanged(state) {
@@ -54,18 +64,32 @@ export class AppNavTop extends connect(store)(LitElement) {
       this.contentElement.style.minHeight 
         = `calc(100% - ${this.bannerElement ? this.bannerElement.offsetHeight : 0}px)`;
     }, 0);
+
+    if (this.isMobile) {
+      this.drawer = MDCDrawer.attachTo(this.drawerElement);
+    } else {
+      delete this.drawer;
+    }
   }
 
   firstUpdated() {
     this.topAppBar = new MDCTopAppBar(this.topAppBarElement);
 
     this.list = MDCList.attachTo(this.listElement);
-    // this.drawer = MDCDrawer.attachTo(this.drawerElement);
-    // this.drawer.open = true;
 
     // Attact Ripple
     [...this.buttonElements, ...this.listItems].forEach(buttonElement => {
       MDCRipple.attachTo(buttonElement);
+    });
+  }
+
+  handleToggleMenu() {
+    this.drawer.open = !this.drawer.open;
+  }
+
+  excuteQueryMatchers() {
+    installMediaQueryWatcher(`(max-width: 600px)`, isMobile => {
+      this.isMobile = isMobile;
     });
   }
 
@@ -74,6 +98,14 @@ export class AppNavTop extends connect(store)(LitElement) {
       <header class="mdc-top-app-bar app-bar mdc-elevation--z4">
         <div class="mdc-top-app-bar__row">
           <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+            ${this.isMobile
+              ? html`
+                <button
+                  class="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button"
+                  @click=${this.handleToggleMenu}
+                >menu</button>
+              `
+              : html``}
             <a href="/home" class="mdc-button mdc-button--unelevated header-text mdc-typography--body2">Birdless Sky</a>
           </section>
           <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end top-bar-sub-item" role="toolbar">
@@ -90,14 +122,20 @@ export class AppNavTop extends connect(store)(LitElement) {
           ? html`<app-banner></app-banner>`
           : html``}
         <div class="drawer-frame-root">
-          <aside class="mdc-drawer">
+          <aside
+            class=${classMap({
+              "mdc-drawer--modal": this.isMobile,
+              "mdc-drawer--modal__mobile": this.isMobile,
+              "mdc-drawer": true
+            })}
+          >
             <div class="mdc-drawer__content">
-              <div class="mdc-list">
-                <a class="mdc-list-item" href="/home">
+              <nav class="mdc-list">
+                <a class="mdc-list-item" href="/home" tabindex="0">
                   <i class="material-icons mdc-list-item__graphic">book</i>
                   <span class="mdc-list-item__text">Blog</span>
                 </a>
-                <a class="mdc-list-item" href="/category">
+                <a class="mdc-list-item ${classMap({ "mdc-list-item--activated": this.isMobile })}" href="/category">
                   <i class="material-icons mdc-list-item__graphic">category</i>
                   <span class="mdc-list-item__text">Category</span>
                 </a>
@@ -109,9 +147,10 @@ export class AppNavTop extends connect(store)(LitElement) {
                   <i class="material-icons mdc-list-item__graphic">info</i>
                   <span class="mdc-list-item__text">About me</span>
                 </a>
-              </div>
+              </nav>
             </div>
           </aside>
+          <div class="mdc-drawer-scrim"></div>
           <main class="main-content" id="main-content">
             <slot></slot>
           </main>
