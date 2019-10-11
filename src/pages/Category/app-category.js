3fc57fb711c58a73d06cc4ae93be3477838a8c2c
quanghuy1242 <katygaga124@gmail.com>
2019-10-11 22:59:12 +0700

    category page

--- src/pages/Category/app-category.js
+++ src/pages/Category/app-category.js
@@ -1,19 +1,63 @@
 import { LitElement, html, customElement, property, unsafeCSS, css } from 'lit-element';
+import { connect, updateMetadata } from 'pwa-helpers';
+import { mdcTypographyStyles } from '../../sharestyles';
 import style from './app-category.scss';
+import { store } from '../../store';
+import { fetchAllCategories } from '../../worker/worker.instance';
+
+import '../../components/CategoryItem/app-category-item';
+import '../../components/CircularProgress/app-circular-progress';
 
 @customElement('app-category')
-export class AppCategory extends LitElement {
+export class AppCategory extends connect(store)(LitElement) {
   @property({ type: Array }) categories;
+  @property({ type: Boolean }) isPending = false;
 
   static get styles() {
     return [
+      mdcTypographyStyles,
       css`${unsafeCSS(style)}`,
     ];
   }
 
+  stateChanged(state) {
+    this.categories = state.category.categories;
+    this.isPending = state.category.isPending;
+  }
+
+  firstUpdated() {
+    fetchAllCategories();
+  }
+
+  updated() {
+    updateMetadata({
+      title: 'Categories',
+      description: `Các thể loại`,
+      url: window.location.href
+    });
+  }
+
   render() {
     return html`
-      <div>Category page</div>
+      <div class="wrapper">
+        <div class="banner">
+          <div class="mdc-typography--headline2">Categories</div>
+        </div>
+        <div class="category-list">
+          ${!this.isPending
+            ? html`
+              ${this.categories?.map(category => html`
+                <app-category-item
+                  .name=${category.name}
+                  .image=${category.imgUrl}
+                ></app-category-item>
+              `)}
+            `
+            : html`
+              <app-circular-progress size="xlarge" center style="margin: 2rem 0"></app-circular-progress>
+            `}
+        </div>
+      </div>
     `;
   }
 }
