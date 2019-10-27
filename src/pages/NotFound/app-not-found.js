fb646752292539a0d52fb0ca06e3ba505b35b46a
Quang Huy <quanghuy1242@gmail.com>
2019-10-28 00:28:47 +0700

    styling app-not-found page

--- src/pages/NotFound/app-not-found.js
+++ src/pages/NotFound/app-not-found.js
@@ -1,10 +1,24 @@
-import { LitElement, html, css, property, customElement } from 'lit-element';
+import { LitElement, html, css, customElement, unsafeCSS } from 'lit-element';
+import { mdcTypographyStyles, mdcButtonStyles } from '../../sharestyles';
+import style from './app-not-found.scss';
 
 @customElement('app-not-found')
 export class AppMain extends LitElement {
+  static get styles() {
+    return [
+      mdcTypographyStyles,
+      mdcButtonStyles,
+      css`${unsafeCSS(style)}`
+    ];
+  }
+
   render() {
     return html`
-      <p>404</p>
+      <div class="wrapper">
+        <div class="mdc-typography--headline1">404</div>
+        <div class="mdc-typography--body2">Nội dung bạn tìm kiếm không được tìm thấy</div>
+        <a class="mdc-button mdc-button--raised" href="/home">Homepage</a>
+      </div>
     `;
   }
 }
